01a6ace25f6547ed7aacd554965eaea6c1731b9d
Kalai <96744855+kalai12334@users.noreply.github.com>
2024-03-15 11:35:46 -0400

    comment removed in login file

--- src/test/java/LoginStepDefinitions/LoginStepDefitions.java
+++ src/test/java/LoginStepDefinitions/LoginStepDefitions.java
@@ -1,8 +1,6 @@
 
   package LoginStepDefinitions;
-  
-//Login step defintion file added
-  
+    
   import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
